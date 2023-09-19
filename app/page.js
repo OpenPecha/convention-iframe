14a0bc4d4577ea13a4b15508799c12b4deb882a1
Tashi Tsering <43548581+ta4tsering@users.noreply.github.com>
2023-09-19 14:54:58 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Sidebar from "./sidebar";
 import { useState, useEffect } from "react";
+import WelcomeMessage from "./welcomeMessage";
 export default function Home({ searchParams }) {
 
   const { base, session } = searchParams;
@@ -9,11 +10,13 @@ export default function Home({ searchParams }) {
 
   return (
     <>
+
       <iframe
         src={`https://work.pecha.tools/${base}/?session=${session}`}
         className="w-screen h-screen"
       />
       <Sidebar>
+        <WelcomeMessage />
         <iframe className="h-full" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT5gDbwko0iKrR46GjJgsmMgGQXc6EiC9xBugl8BvV66pPz-czBeyO1DfVsti0jg-EJqTZ7dSUCu_qC/pubhtml?widget=true&amp;headers=false"></iframe>
       </Sidebar>
     </>
