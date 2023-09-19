276c93addd677ffb943b93f51e8eda943f30fe11
Tashi Tsering <43548581+ta4tsering@users.noreply.github.com>
2023-09-19 14:32:06 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import Sidebar from "./sidebar";
 import { useState, useEffect } from "react";
-import WelcomeMessage from "./welcomeMessage";
 export default function Home({ searchParams }) {
 
   const { base, session } = searchParams;
@@ -15,7 +14,6 @@ export default function Home({ searchParams }) {
         className="w-screen h-screen"
       />
       <Sidebar>
-        <WelcomeMessage/>
         <iframe className="h-full" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT5gDbwko0iKrR46GjJgsmMgGQXc6EiC9xBugl8BvV66pPz-czBeyO1DfVsti0jg-EJqTZ7dSUCu_qC/pubhtml?widget=true&amp;headers=false"></iframe>
       </Sidebar>
     </>
