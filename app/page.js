5ada00b1a732c331dbaa2e9517d451c76b5a9eae
Tashi Tsering <43548581+ta4tsering@users.noreply.github.com>
2023-09-19 14:25:48 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,13 +10,12 @@ export default function Home({ searchParams }) {
 
   return (
     <>
-
       <iframe
         src={`https://work.pecha.tools/${base}/?session=${session}`}
         className="w-screen h-screen"
       />
       <Sidebar>
-        <WelcomeMessage username={username} workSpace={workSpace} />
+        <WelcomeMessage/>
         <iframe className="h-full" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT5gDbwko0iKrR46GjJgsmMgGQXc6EiC9xBugl8BvV66pPz-czBeyO1DfVsti0jg-EJqTZ7dSUCu_qC/pubhtml?widget=true&amp;headers=false"></iframe>
       </Sidebar>
     </>
