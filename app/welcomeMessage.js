ddf17c35c419fb11b3a432513a4d5dc07674f699
Tashi Tsering <43548581+ta4tsering@users.noreply.github.com>
2023-09-19 14:59:08 +0530

    Update welcomeMessage.js

--- app/welcomeMessage.js
+++ app/welcomeMessage.js
@@ -1,7 +1,11 @@
-export default function WelcomeMessage() {
+export default function WelcomeMessage({ username, workSpace }) {
     return (
       <h3 className="mt-10 mb-2 text-4xl font-semibold text-white">
-        {(
+        {username && workSpace ? (
+          <>
+            {username}! welcome
+          </>
+        ) : (
           <>select the options</>
         )}
       </h3>
