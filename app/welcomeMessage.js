1318e0d4e1930d054fff79c5b29cbc2fc77ed349
Tashi Tsering <43548581+ta4tsering@users.noreply.github.com>
2023-09-19 14:52:59 +0530

    Update welcomeMessage.js

--- app/welcomeMessage.js
+++ app/welcomeMessage.js
@@ -1,9 +1,9 @@
 export default function WelcomeMessage() {
     return (
       <h3 className="mt-10 mb-2 text-4xl font-semibold text-white">
-        (
+        {(
           <>select the options</>
-        )
+        )}
       </h3>
     );
   }
